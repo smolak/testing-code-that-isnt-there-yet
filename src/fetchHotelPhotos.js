03985f1d77bf34ed1b0bfd396cf3967fdd3fb6a2
Jacek Smolak <jacek.smolak@gmail.com>
2017-12-29 16:45:37 +0100

    Add fetching collection from DB

--- src/fetchHotelPhotos.js
+++ src/fetchHotelPhotos.js
@@ -1,3 +1,5 @@
-export default function fetchHotelPhotos() {
+export default function fetchHotelPhotos(dbClient, collectionName) {
+    dbClient.collection(collectionName); // (3)
+
     return [ 'photo-1.jpg', 'photo-2.jpg', 'photo-3.jpg' ];
 }
--- test/unit/src/fetchHotelPhotosSpec.js
+++ test/unit/src/fetchHotelPhotosSpec.js
@@ -1,10 +1,24 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 import fetchHotelPhotos from '../../../src/fetchHotelPhotos';
 
 describe('fetchHotelPhotos', () => {
+    const connectedClientDouble = {
+        collection: sinon.spy()
+    };
+    const collectionName = 'hotels';
+
+    it('should fetch hotels collection from DB', () => {
+        fetchHotelPhotos(connectedClientDouble, collectionName);
+
+        expect(connectedClientDouble.collection)
+            .to.have.been.calledWithExactly('hotels') // (1)
+            .to.have.been.calledOnce;
+    });
+
     it('should return hotel photos collection', () => {
-        const photosCollection = fetchHotelPhotos();
+        const photosCollection = fetchHotelPhotos(connectedClientDouble, collectionName); // (2)
 
         expect(photosCollection).to.deep.equal([
             'photo-1.jpg', 'photo-2.jpg', 'photo-3.jpg'
